4b8e7ee21381d1e16a3e81aba6ac1da1bec13292
Chen Osipov <eobender@gmail.com>
2021-02-23 09:41:14 -0800

    reverted exising nuxt

--- plugins/alert.js
+++
@@ -1,9 +0,0 @@
-import Swal from "vue-sweetalert2";
-import Vue from "vue";
-Vue.use(Swal);
-
-export default ({ app }, inject) => {
-  inject("swal", params => {
-    Swal(params);
-  });
-};
--- plugins/i18n.js
+++
@@ -1,4 +0,0 @@
-export default function({ app }, inject) {
-  const dir = () => app.i18n.locales.find(x => x.code === app.i18n.locale)?.dir;
-  inject("dir", dir);
-}
